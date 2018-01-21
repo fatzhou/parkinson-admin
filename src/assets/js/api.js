6a08cfb262fc0580b98b53cd7d5570e3a8ca6950
fatzhou <fatzhou@tencent.com>
2018-01-21 11:48:09 +0800

    project init

---
+++ src/assets/js/api.js
@@ -0,0 +1,5 @@
+import fetch from './fetch'
+
+export const getUser = ( num ) => fetch('/action_214/user/get_userinfo', {
+	num
+}, 'POST');
---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import FastClick from 'fastclick'
+import store from './store/'
+
+Vue.config.productionTip = false
+
+if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function() {
+        FastClick.attach(document.body);
+    }, false);
+}
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
